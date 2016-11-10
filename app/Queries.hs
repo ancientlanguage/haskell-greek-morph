c9d75ad47ea4d8f0944e98bf1d1a27ec85709689
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2016-11-10 14:10:39 -0800

    Add comment stub

--- app/Queries.hs
+++ app/Queries.hs
@@ -1,1 +1,34 @@
 module Queries where
+
+import Prelude hiding (Word)
+import Grammar.Common
+import Grammar.IO.QueryStage
+import Grammar.Greek.Script.Word
+import Grammar.Greek.Morph.Serialize
+
+{-
+queryStage
+  :: (Show e1, Ord c, Show c)
+  => Round
+  (MilestoneCtx :* e1)
+    e2
+    [MilestoneCtx :* Word]
+    [b]
+  -> (b -> [c])
+  -> QueryOptions
+  -> IO ()
+queryStage a f = queryStageContext 0 a (f . fst)
+
+queryStageContext
+  :: (Show e1, Ord c, Show c)
+  => Int
+  -> Round
+    (MilestoneCtx :* e1)
+    e2
+    [MilestoneCtx :* Word]
+    [b]
+  -> (b :* [b] :* [b] -> [c])
+  -> QueryOptions
+  -> IO ()
+queryStageContext contextSize stg itemQuery qo = readScript >>= queryStageWithContext contextSize stg itemQuery qo id id id
+-}
