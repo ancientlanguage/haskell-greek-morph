2cf6966c008b37484951530c9cd8eeffd383b489
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2016-11-13 07:08:27 -0800

    Add stub adjectives and participles file

---
+++ src/Grammar/Greek/Morph/Smyth/Adjectives.hs
@@ -0,0 +1,24 @@
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+{-# LANGUAGE QuasiQuotes #-}
+
+module Grammar.Greek.Morph.Smyth.Adjectives where
+
+import Grammar.Greek.Morph.QuasiQuoters
+
+-- adjectives =
+--   [ [nounParadigm|
+-- Ἀτρείδης
+-- Ἀτρείδου
+-- Ἀτρείδῃ
+-- Ἀτρείδη-ν
+-- Ἀτρείδη
+
+-- Ἀτρείδᾱ
+-- Ἀτρείδαιν
+
+-- Ἀτρεῖδαι
+-- Ἀτρειδῶν
+-- Ἀτρείδαις
+-- Ἀτρείδᾱς
+--     |]
+--   ]
---
+++ src/Grammar/Greek/Morph/Smyth/Verbs/Participles.hs
@@ -0,0 +1,24 @@
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+{-# LANGUAGE QuasiQuotes #-}
+
+module Grammar.Greek.Morph.Smyth.Verbs.Participles where
+
+import Grammar.Greek.Morph.QuasiQuoters
+
+-- participles =
+--   [ [nounParadigm|
+-- Ἀτρείδης
+-- Ἀτρείδου
+-- Ἀτρείδῃ
+-- Ἀτρείδη-ν
+-- Ἀτρείδη
+
+-- Ἀτρείδᾱ
+-- Ἀτρείδαιν
+
+-- Ἀτρεῖδαι
+-- Ἀτρειδῶν
+-- Ἀτρείδαις
+-- Ἀτρείδᾱς
+--     |]
+--   ]
