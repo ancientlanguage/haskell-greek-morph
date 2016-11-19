4955c96a8dff312236f2718c8880cb4b3528d0e3
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2016-11-19 00:02:09 -0600

    Add ending form map

--- src/Grammar/Greek/Morph/Paradigm/Maps.hs
+++ src/Grammar/Greek/Morph/Paradigm/Maps.hs
@@ -1,11 +1,16 @@
 module Grammar.Greek.Morph.Paradigm.Maps where
 
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
 import Grammar.Common
 import Grammar.Greek.Morph.Types
 import Grammar.Greek.Morph.Paradigm.Types
 import Grammar.Greek.Morph.Smyth.Nouns.Declension1
 import Grammar.Greek.Morph.Smyth.Nouns.Declension2
 import Grammar.Greek.Morph.Smyth.Nouns.Declension3
+
+endingFormMap :: Map [Phoneme] [ParadigmForm]
+endingFormMap = Map.fromList endingFormGroups
 
 endingFormGroups :: [[Phoneme] :* [ParadigmForm]]
 endingFormGroups
