bb53d1140735b91648c902706bd9f16a653cb940
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2016-11-18 18:05:46 -0700

    Add declension 2 to all morph forms

--- src/Grammar/Greek/Morph/Paradigm/Maps.hs
+++ src/Grammar/Greek/Morph/Paradigm/Maps.hs
@@ -4,9 +4,10 @@ import Grammar.Common
 import Grammar.Greek.Morph.Types
 import Grammar.Greek.Morph.Paradigm.Types
 import Grammar.Greek.Morph.Smyth.Nouns.Declension1
+import Grammar.Greek.Morph.Smyth.Nouns.Declension2
 
 endingFormGroups :: [[Phoneme] :* [ParadigmForm]]
 endingFormGroups
   = groupPairs
   $ fmap (\f -> (paradigmEndingEnding . paradigmFormEnding) f :^ f)
-  $ declension1Forms
+  $ declension1Forms ++ declension2Forms
